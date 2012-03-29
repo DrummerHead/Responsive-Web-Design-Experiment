c6c2442de47677ea1b6d97717281b81aa9dfc5d1
DrummerHead <drummerhead@gmail.com>
2012-03-29 16:17:54 -0300

    add to github

---
+++ gedrag.js
@@ -0,0 +1,21 @@
+/*
+var coso = $('ol'),
+  metele = '';
+for(var i=16; i<=129; i++) {
+  metele += '<li>'+i*20+'</li>\n';
+}
+coso.append(metele);
+*/
+/*
+var jojo='',
+  num = 242,
+  width = 320;
+for (var i=0; i <= 3000; i++) {
+  jojo += '@media ( min-width:'+ (width+i*20) +'px ) { #nl{top: '+(num-i*32)+'px;} }\n';
+}
+console.log(jojo);
+*/
+
+/*
+@media ( min-width:320px ) { #nl{top: 242px;} }
+*/
